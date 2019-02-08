47efe4e1756ed29d944256e34e292d1aa2c942d7
pancham <panchamrb@phonepe.com>
2019-02-08 12:46:15 +0530

    Working with mock server

--- service/edge.go
+++ service/edge.go
@@ -33,6 +33,8 @@ func (e *Edge) Proxy(serviceDef core.ServiceDef, servicePath core.ServicePath, r
 	if err != nil {
 		fmt.Printf("Error: in calling %s => %+v", url, err)
 	} else {
-		fmt.Printf("Made a call to %s and got response: %d\n", servicePath.Path, resp.StatusCode)
+		if resp != nil {
+			fmt.Printf("Made a call to %s and got response: %+v\n", servicePath.Path, resp.StatusCode)	
+		}
 	}
 }
